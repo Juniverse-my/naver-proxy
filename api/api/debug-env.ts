3a34bf372eeed1af0280da1ea89fe468c075b839
Juniverse-my <ucanyj@gmail.com>
2025-09-27 23:17:58 +0900

    Delete api/api directory

--- api/api/debug-env.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const id = process.env.NAVER_CLIENT_ID || "";
-  const sec = process.env.NAVER_CLIENT_SECRET || "";
-  res.status(200).json({ hasId: !!id, idLength: id.length, hasSecret: !!sec, secretLength: sec.length });
-}
